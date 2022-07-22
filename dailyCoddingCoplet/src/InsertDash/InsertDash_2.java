2a2a898c902d1510c36c431b094e9af2055ee048
WiseJade <104193237+WiseJade@users.noreply.github.com>
2022-07-22 09:46:20 +0900

    10번 InsertDash 풀이 추가

---
+++ dailyCoddingCoplet/src/InsertDash/InsertDash_2.java
@@ -0,0 +1,24 @@
+package InsertDash;
+
+import java.util.*;
+
+public class InsertDash_2 {
+    public String insertDash(String str) {
+        char[] charArray = str.toCharArray();
+        List<Character> list = new ArrayList<>();
+        for (char c : charArray) {
+            list.add(c);
+        }
+
+        for (int i = 1; i < list.size(); i++) {
+            if ((list.get(i - 1) - '0') % 2 == 1 && (list.get(i) - '0') % 2 == 1)
+                list.add(i, '-');
+        }
+        String result = list.toString();
+        result = result.replace("]", "");
+        result = result.replace("[", "");
+        result = result.replace(", ", "");
+
+        return result;
+    }
+}
