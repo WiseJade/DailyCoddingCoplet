cce86fd5ef12aec1d6c33deb7a51f6afd7951c5a
WiseJade <jeonhr861@gmail.com>
2022-08-08 20:04:55 +0900

    38번 MergeSort 추가

---
+++ dailyCoddingCoplet/src/MergeSort.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class MergeSort {
+    public int[] mergeSort(int[] arr) {
+        // TODO :
+        if (arr.length < 2) {
+            return arr;
+        }
+        int middle = arr.length / 2;
+        int[] left = Arrays.copyOfRange(arr, 0, middle);
+        int[] right = Arrays.copyOfRange(arr, middle, arr.length);
+        left = mergeSort(left);
+        right = mergeSort(right);
+        int[] result = new int[left.length + right.length];
+        int leftIdx = 0;
+        int rightIdx = 0;
+        for (int i = 0; i < result.length; i++) {
+            if (leftIdx >= left.length) {
+                System.arraycopy(right, rightIdx, result, i, right.length - rightIdx);
+                break;
+            } else if (rightIdx >= right.length) {
+                System.arraycopy(left, leftIdx, result, i, left.length - leftIdx);
+                break;
+            } else if(left[leftIdx] > right[rightIdx]) {
+                result[i] = right[rightIdx];
+                rightIdx++;
+            } else {
+                result[i] = left[leftIdx];
+                leftIdx++;
+            }
+        }
+        return result;
+    }
+}
